bb87dd2fa1c934cdcab60fdafbbe5e0e34e0a113
202296 <mouhamedkoko996@gmail.com>
2023-01-10 09:16:28 +0000

    changed the code of last modified

--- scripts/portal.js
+++ scripts/portal.js
@@ -1,14 +1,8 @@
-const fullDate = new Date();
-const date = fullDate.getDate();
-const month = fullDate.getMonth() + 1;
-const hour = fullDate.getHours();
-const min = fullDate.getMinutes();
-const sec = fullDate.getSeconds();
-const fullYear = fullDate.getFullYear();
+const fullYear = new Date().getFullYear();
 
 const sumb = document.querySelector('footer');
 sumb.innerHTML = `&copy; ${fullYear} | Koko Mouhamed Jean Patrick | Ivory Coast`;
 
 const para = document.createElement('p');
-para.textContent = `Last Updated: ${date}/${month}/${fullYear} ${hour}:${min}:${sec}`;
+para.textContent = `Last Updated: ${document.lastModified}`;
 sumb.appendChild(para)
