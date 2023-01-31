1451f34d36136c8ce20286606546f5e6c3d99ff6
202296 <mouhamedkoko996@gmail.com>
2023-01-31 19:52:41 +0000

    heading adjustment

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -4,7 +4,7 @@ const monthName = d.toLocaleString('en-us', { month: "long" });
 const date = d.getDate();
 const year = d.getFullYear();
 const selHead = document.getElementById('date');
-const lastModifDate = document.createElement('p');
+const lastModifDate = document.createElement('h2');
 lastModifDate.innerHTML = `${day}, ${date} ${monthName} ${year}`
 selHead.appendChild(lastModifDate)
 
@@ -23,14 +23,14 @@ x.onclick = toggleMenu;
 const fullYear = new Date().getFullYear();
 const sumb = document.getElementById('info');
 sumb.innerHTML = `&copy; ${fullYear} Spirit of Enterprise <br> Koko Mouhamed Jean Patrick <br> WDD 230 Project`;
-const para = document.createElement('p');
+const para = document.createElement('h4');
 para.textContent = `Last Modification: ${document.lastModified}`;
 sumb.appendChild(para)
 
 const sumbo = document.getElementById('medium');
 sumbo.innerHTML = `&copy; ${fullYear} Spirit of Enterprise | Koko Mouhamed Jean Patrick | WDD 230 Project |`;
 
-const parag = document.createElement('p');
+const parag = document.createElement('h4');
 parag.textContent = `Last Modification: ${document.lastModified}`;
 sumbo.appendChild(parag)
 const sumbol = document.getElementById('large');
