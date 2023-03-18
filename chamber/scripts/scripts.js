375c0edaa03413efee030dea7c5bac594a9251ee
202296 <mouhamedkoko996@gmail.com>
2023-03-18 23:11:08 +0000

    changed my chamber website name

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -31,19 +31,19 @@ x.onclick = toggleMenu;
 
 const fullYear = new Date().getFullYear();
 const sumb = document.getElementById('info');
-sumb.innerHTML = `&copy; ${fullYear} Spirit of Enterprise <br> Koko Mouhamed Jean Patrick <br> WDD 230 Project`;
+sumb.innerHTML = `&copy; ${fullYear} Abidjan Chamber of Commerce <br> Koko Mouhamed Jean Patrick <br> WDD 230 Project`;
 const para = document.createElement('h4');
 para.textContent = `Last Modification: ${document.lastModified}`;
 sumb.appendChild(para)
 
 const sumbo = document.getElementById('medium');
-sumbo.innerHTML = `&copy; ${fullYear} Spirit of Enterprise | Koko Mouhamed Jean Patrick | WDD 230 Project |`;
+sumbo.innerHTML = `&copy; ${fullYear} Abidjan Chamber of Commerce | Koko Mouhamed Jean Patrick | WDD 230 Project |`;
 
 const parag = document.createElement('h4');
 parag.textContent = `Last Modification: ${document.lastModified}`;
 sumbo.appendChild(parag)
 const sumbol = document.getElementById('large');
-sumbol.innerHTML = `&copy; ${fullYear} Spirit of Enterprise | Koko Mouhamed Jean Patrick | WDD 230 Project | Last Modification: ${document.lastModified}`;
+sumbol.innerHTML = `&copy; ${fullYear} Abidjan Chamber of Commerce | Koko Mouhamed Jean Patrick | WDD 230 Project | Last Modification: ${document.lastModified}`;
 
 document.querySelectorAll('.nav-link').forEach(link => {
   if(link.href === window.location.href){
