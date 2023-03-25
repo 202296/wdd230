efd6927ce54db3c2759cc02d1424ac080322b378
202296 <mouhamedkoko996@gmail.com>
2023-03-25 16:46:37 +0000

    changed the website plan

--- final-website-project/scripts/final.js
+++ final-website-project/scripts/final.js
@@ -109,13 +109,6 @@ closeModalButtons.addEventListener('click', () => {
   modal.close();
 })
 
-const remove = document.querySelectorAll('.close-button');
-for(let i=0; i<remove.length; i++){
-  remove[i].addEventListener('click', function() {
-    remove[i].parentElement.style.display = 'none';
-  })
-}
-
 
 const storedTotalDrinks = localStorage.getItem('totalDrinks');
 const result = document.getElementById('formdata')
