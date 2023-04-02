80215bb0c5eb63ee6a2f51d2d625f8ecc25ba933
202296 <mouhamedkoko996@gmail.com>
2023-04-02 17:22:46 +0000

    delected unecessary file and picture

--- final-website-project/scripts/slide.js
+++ final-website-project/scripts/slide.js
@@ -1,21 +1,9 @@
-const swiper = new Swiper('.swiper', {
-    // autoplay: {
-    //     delay: 3000,
-    //     disableOnInteraction: false,
-    // },
+var swiper = new Swiper(".box", {
+  spaceBetween: 30,
+  centeredSlides: true,
 
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-  
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
